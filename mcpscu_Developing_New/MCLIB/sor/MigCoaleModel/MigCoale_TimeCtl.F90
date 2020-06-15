77dec4f9948ebf6d51ade4009332aa0eb72fe35c
zhailei <zhl@stu.scu.edu.cn>
2020-06-15 10:37:05 +0800

    fix the bug and publish NNDR_2020_06_15

--- mcpscu_Developing_New/MCLIB/sor/MigCoaleModel/MigCoale_TimeCtl.F90
+++ mcpscu_Developing_New/MCLIB/sor/MigCoaleModel/MigCoale_TimeCtl.F90
@@ -113,10 +113,6 @@ module MIGCOALE_TIMECTL
             case(mp_SelfAdjustlStep_NNDR_S4)
                 TSTEP = max(minval(Dev_Boxes%dm_ClusterInfo_GPU%dm_MinTSteps),dble(Host_SimuCtrlParam%LowerLimitTime))
 
-
-                write(*,*)  "minval(Dev_Boxes%dm_ClusterInfo_GPU%dm_MinTSteps) ",minval(Dev_Boxes%dm_ClusterInfo_GPU%dm_MinTSteps)
-                write(*,*) "TSTEP",TSTEP
-
             case default
                 write(*,*) "MCPSCUERROR: Unknown strategy to update time step :",Host_SimuCtrlParam%UPDATETSTEPSTRATEGY
                 pause
