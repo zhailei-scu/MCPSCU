c4413083d7a170f75d1032ff454c019749da7efa
zhailei <zhl@stu.scu.edu.cn>
2020-07-07 10:54:13 +0800

    normal operation

--- mcpscu_Developing_New/MCLIB/sor/Common/MCLIB_CONSTANTS.F90
+++ mcpscu_Developing_New/MCLIB/sor/Common/MCLIB_CONSTANTS.F90
@@ -45,6 +45,7 @@ module MCLIB_CONSTANTS
     integer,parameter::mp_SelfAdjustlStep_NNDR = 3         ! the time step is determined by the NNDR algorithm
     integer,parameter::mp_SelfAdjustlStep_NNDR_S4 = 4      ! the time step is determined by the NNDR algorithm S4
     integer,parameter::mp_SelfAdjustlStep_NNDR_LastPassage = 5      ! the time step is determined by the NNDR algorithm LastPassage
+    integer,parameter::mp_SelfAdjustlStep_NNDR_Integer = 6      ! the time step is determined by the NNDR algorithm but is integer times of lower limit
 
     integer,parameter::mp_UpdateStatisFlag_ByIntervalSteps = 0
     integer,parameter::mp_UpdateStatisFlag_ByIntervalRealTime = 1
