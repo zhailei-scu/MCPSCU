ced06e3d08db49b109104e8e278f3995fa584903
zhailei <zhl@stu.scu.edu.cn>
2021-05-03 23:10:19 +0800

    normal operation

--- mcpscu_Developing_New/MCLIB/HybridMCRTModel/HybridMCRTModel.mk
+++ mcpscu_Developing_New/MCLIB/HybridMCRTModel/HybridMCRTModel.mk
@@ -22,17 +22,17 @@ sor := $(MCLIBDIRS)$(Segment)$(objname)
 ifeq ($(origin LIBDIRD), undefined)
 LIBDIRD := $(mcworkspace)$(Segment)LIB$(Segment)$(ConfigName)
 endif
-incdir := $(LIBDDIRD)
+incdir := $(LIBDIRD)
 
 #target directories
-tgt := $(LIBDDIRD)
+tgt := $(LIBDIRD)
 
 #target lib name
 libname := libMC_$(objname).$(LIB_EXT)
 
 #######################################################
-nlist	:=	HybridMCRT_TYPEDEF_Collections	\
-			HybridMCRT_TYPEDEF_EVCCluster	\
+nlist	:=	HybridMCRT_TYPEDEF_EVCCluster	\
+			HybridMCRT_TYPEDEF_Collections	\
 			HybridMCRT_Evolution_GPU
 
 objects		:= 	$(foreach n, $(nlist), $(tgt)$(Segment)$(n).o)
@@ -41,3 +41,20 @@ ffiles		:=	$(foreach n, $(nlist), $(tgt)$(Segment)$(n).f)
 fFiles		:=	$(foreach n, $(nlist), $(tgt)$(Segment)$(n).F)
 F90Files	:=	$(foreach n, $(nlist), $(tgt)$(Segment)$(n).F90)
 
+#######################################################
+$(libname) : $(objects)
+	ar -rcs $(libname) $(objects)
+	mv $(libname) $(tgt)
+
+$(tgt)$(Segment)HybridMCRT_TYPEDEF_EVCCluster.o : $(sor)$(Segment)HybridMCRT_TYPEDEF_EVCCluster.F90
+	$(comp) -c $(oflags_this) -I$(incdir) -module $(tgt) $< -o $@
+
+$(tgt)$(Segment)HybridMCRT_TYPEDEF_Collections.o : $(sor)$(Segment)HybridMCRT_TYPEDEF_Collections.F90 \
+												   $(tgt)$(Segment)HybridMCRT_TYPEDEF_EVCCluster.o 
+	$(comp) -c $(oflags_this) -I$(incdir) -module $(tgt) $< -o $@
+
+
+
+
+$(tgt)$(Segment)HybridMCRT_Evolution_GPU.o : $(sor)$(Segment)HybridMCRT_Evolution_GPU.F90
+	$(comp) -c $(oflags_this) -I$(incdir) -module $(tgt) $< -o $@
