44b806c1f07fd00af374e972743628f3bd0e267b
zhailei-scu <zhaileiytp@163.com>
2026-08-16 15:58:50 +0800

    add the docs for gitlab ce

---
+++ docs/source/conf.py
@@ -0,0 +1,94 @@
+import os
+import subprocess
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MCPSCU Documents'
+copyright = '2017-2026, Sichuan university'
+author = 'zhailei-scu(Lei Zhai)'
+
+release = "latest"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinxcontrib.mermaid",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+html_context = {}
+
+html_context["display_manual_versions"] = True
+
+html_context["current_version"] = "release"
+
+html_context["versions"] = [
+    (
+        "develop",
+        "/develop/"
+    ),
+    (
+        "stable",
+        "/stable/"
+    ),
+    (
+        "release",
+        "/"
+    ),
+]
+
+html_context["downloads"] = [
+    (
+        "PDF",
+        "downloads/MCPSCU.pdf"
+    ),
+]
+
+try:
+    git_commit = subprocess.check_output(
+        [
+            "git",
+            "rev-parse",
+            "--short",
+            "HEAD"
+        ]
+
+    ).decode().strip()
+except:
+    git_commit="unknown"
+
+html_context["git_commit"] = git_commit
+
+html_context["release"] = "release"
+
+myst_enable_extensions = [
+    "amsmath",
+
+    "colon_fence",
+
+    "deflist",
+
+    "html_image",
+]
+
+copybutton_prompt_text = r">>> |\.\.\. "
+
+mermaid_version = "10.6.1"
---
+++ docs/source/index.md
@@ -0,0 +1,13 @@
+MCPSCU:GPU based Kinetic Monte Carloi (KMC) simulation package for Sichuan University (based on migration-coalescence diffusion model for objects migration-coalescence diffusion-evolutions in materials).<br/>
+
+SCOPE: Material micro-evolution, kinetic diffusion-reaction simulation.<br/>
+
+Author: Lei Zhai(<zhaileiytp@163.com>);Qing Hou(<qhou@scu.edu.cn>).<br/>
+
+Institute:Key Lab of Radiation Physics and Technology, Institute of Nuclear Science and Technology, Sichuan University, Chengdu 610064, People's Republic of China.<br/>
+
+Please ref "Lei Zhai, Chaoqiong Ma, Jiechao Cui and Qing Hou, GPU-based acceleration of Monte Carlo simulations for migration-coalescence evolution of gas bubbles in materials". Modelling Simul. Mater. Sci. Eng. 2019. 27 055008".<https://iopscience.iop.org/article/10.1088/1361-651X/ab1d14> </br>
+
+Email: <zhaileiytp@163.com>  or  <lei.zhai@hfcim.com>  </br>
+
+License: MIT License (There are no limitation for anyone to use/modify/sale of this code, be happy to use this code).
